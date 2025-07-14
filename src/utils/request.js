fc1c732eafaaed09e8158f9e065766ff05a810f5
ledat <ledat1010204@gmail.com>
2025-07-14 21:04:43 +0700

    chỉnh sửa domain

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,78 +3,79 @@ import Cookies from "js-cookie";
 import { tokenExpiredAccount } from "../actions/auth";
 import store from "../store";
 
-const API_DOMAIN = "http://localhost:5000/api/admin/";
+const API_DOMAIN =
+  "https://api-project-product-management.vercel.app/api/admin/";
 
 const axiosInstance = axios.create();
 
-axiosInstance.interceptors.request.use((config) => {
+axiosInstance.interceptors.request.use(
+  (config) => {
     if (config.data instanceof FormData) {
-        config.headers["Content-Type"] = "multipart/form-data";
+      config.headers["Content-Type"] = "multipart/form-data";
     } else {
-        config.headers["Content-Type"] = "application/json";
+      config.headers["Content-Type"] = "application/json";
     }
     return config;
-}, (error) => {
+  },
+  (error) => {
     return Promise.reject(error);
-});
+  }
+);
 
-const EXCLUDED_URLS = [
-    "accounts/login"
-];
+const EXCLUDED_URLS = ["accounts/login"];
 
 axiosInstance.interceptors.request.use((config) => {
-    const token = Cookies.get("accessToken");
+  const token = Cookies.get("accessToken");
 
-    // Kiểm tra URL có trong danh sách loại trừ không
-    const isExcluded = EXCLUDED_URLS.some((url) => config.url.includes(url));
+  // Kiểm tra URL có trong danh sách loại trừ không
+  const isExcluded = EXCLUDED_URLS.some((url) => config.url.includes(url));
 
-    if (token && !isExcluded) {
-        config.headers.authorization = `Bearer ${token}`;
-    };
+  if (token && !isExcluded) {
+    config.headers.authorization = `Bearer ${token}`;
+  }
 
-    return config;
+  return config;
 });
 
 // ✅ Interceptor cho response
 axiosInstance.interceptors.response.use(
-
-    (response) => {
-        // Trường hợp server trả về code 401 trong dữ liệu
-        if (response?.data?.code === 401) {
-            store.dispatch(tokenExpiredAccount());
-        }
-        return response;
-    },
-    (error) => {
-        // Trường hợp response lỗi 401 từ status code
-        if (error?.response?.status === 401) {
-            // console.log("Token hết hạn (status 401)");
-            store.dispatch(tokenExpiredAccount());
-        }
-        return Promise.reject(error);
+  (response) => {
+    // Trường hợp server trả về code 401 trong dữ liệu
+    if (response?.data?.code === 401) {
+      store.dispatch(tokenExpiredAccount());
     }
+    return response;
+  },
+  (error) => {
+    // Trường hợp response lỗi 401 từ status code
+    if (error?.response?.status === 401) {
+      // console.log("Token hết hạn (status 401)");
+      store.dispatch(tokenExpiredAccount());
+    }
+    return Promise.reject(error);
+  }
 );
 
 export const get = async (path) => {
-    const response = await axiosInstance.get(API_DOMAIN + path);
-    const result = response.data;
-    return result;
+  const response = await axiosInstance.get(API_DOMAIN + path);
+  const result = response.data;
+  return result;
 };
 
 export const patch = async (path, option) => {
-    const response = await axiosInstance.patch(API_DOMAIN + path, option);
-    const result = response.data;
-    return result;
+  const response = await axiosInstance.patch(API_DOMAIN + path, option);
+  const result = response.data;
+  return result;
 };
 
 export const del = async (path) => {
-    const response = await axiosInstance.delete(API_DOMAIN + path);
-    const result = response.data;
-    return result;
+  const response = await axiosInstance.delete(API_DOMAIN + path);
+  const result = response.data;
+  return result;
 };
 
 export const post = async (path, option) => {
-    const response = await axiosInstance.post(API_DOMAIN + path, option);
-    const result = response.data;
-    return result;
+  const response = await axiosInstance.post(API_DOMAIN + path, option);
+  const result = response.data;
+  return result;
 };
